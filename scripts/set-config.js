98d0aad6c9814a6ccf0174d3c1ace771d39bbc3c
Mathias Eek <mathias.eek@statnett.no>
2025-08-22 17:03:35 +0200

    feat: add set-config script from library

---
+++ scripts/set-config.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable no-undef */
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { spawn } from 'child_process';
+
+// Parse arguments using yargs
+const argv = yargs(hideBin(process.argv))
+  .usage('Usage: $0 --set <value>')
+  .option('set', {
+    alias: 's',
+    type: 'string',
+    demand: true,
+    description: 'The suffix to add to the config',
+  })
+  .help().argv;
+
+const suffix = '-' + argv.set;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const customSuffixFileName = 'custom-suffix.json';
+const configFilePath = path.resolve(__dirname, '../dist/' + customSuffixFileName);
+
+fs.writeFileSync(configFilePath, JSON.stringify(suffix, null, 2));
+console.log(`custom-suffix config updated successfully\nnew suffix set: "${suffix}"`);
